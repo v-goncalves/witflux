3fa168bdc5be464a0bc3e9f285a397363bfde4ac
Vítor Gonçalves <vitor.goncalves@wit-software.com>
2018-11-11 21:56:17 +0000

    fix prod endpont "getMovieDetails"

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,7 +8,7 @@ export const environment: Environment = {
     getConfiguration: 'https://api.themoviedb.org/3/configuration?api_key=${apiKey}',
     getMostPopularMovies: `https://api.themoviedb.org/3/trending/movie/week?api_key=${apiKey}`,
     getRecommendedSeries: `https://api.themoviedb.org/3/trending/tv/week?api_key=${apiKey}`,
-    getMovieDetails: 'https://api.themoviedb.org/3/movie/{{ID-DO-FILME}}?api_key=${apiKey}&language=en-US&append_to_response=credits',
+    getMovieDetails: `https://api.themoviedb.org/3/movie/<id>?api_key=${apiKey}&language=en-US&append_to_response=credits`,
     getSeriesDetails: `https://api.themoviedb.org/3/tv/<id>?api_key=${apiKey}&language=en-US&append_to_response=credits`
   }
 };
